01cf3d0f5b2f87e9f4e00c1aaaa492b4aaf57d1f
jimmylu1 <jimmy88lu@gmail.com>
2019-04-11 22:46:11 -0700

    Implement solution for linked lists
    Start implementing tree.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,62 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    /*
+    node.prev = list.tail
+    list.tail = value
+    when a node is added to tail, new node should become list.tail
+    previous tail should now have a node.next equal list.tail instead of null
+    */
+    if (list.tail !== null) {
+      /*
+      condition to determine if there is something to add the new node to tail
+      */
+      list.tail.next = node;
+      list.tail = node;
+    } else {
+      list.head = node;
+      //list.head.next?
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var newHead = list.head.next;
+    var val = list.head.value;
+    list.head = newHead;
+    return val;
   };
 
   list.contains = function(target) {
-  };
+    /*
+    search through all nodes to find target/value
+    should return true if target/value is in the list
+    should return false if target/value is NOT in the list
 
+    search through list
+      start search from head
+        recursive function until target is reached
+          base case
+            if (target === value) {
+              return true;
+            }
+        return false if target is not found
+    */
+
+   list.currNode = list.head;
+     while (list.currNode !== list.tail) {
+       if (list.currNode.value === target) {
+         return true;
+       } else {
+         list.currNode = list.currNode.next;
+       }
+     }
+     if (list.currNode.value === target) {
+       return true;
+     }
+     return false;  
+   };
   return list;
 };
 
@@ -20,9 +68,53 @@ var Node = function(value) {
 
   node.value = value;
   node.next = null;
+  // node.prev = null;
 
   return node;
 };
+    
+
+/*
+REFACTOR CODE BELOW
+*/
+    // for (var key in list) {
+    //   var node = list[key];
+    //   if (typeof list[key] === 'object') {
+
+    //     if (node.value === target) {
+    //       return true;
+    //     } 
+    //     if (node !== list.tail) {
+    //       list.contains(node.next);
+    //     } 
+    //     return false;
+    //   }
+    // }
+
+  //   var recurse = function (node, target) {
+
+  //     if (node.value === target.value) {
+  //       return true; 
+  //     } 
+  //     else if (node.next) {
+  //       recurse(node.next);
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  //   recurse(list, target);
+  
+
+
+
+
+// for (var key in list.head) {
+      //   if (list[key].value === target) {
+      //     return true;
+      //   } else {
+      //     list.contains(list)
+      //   }
+      // }
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,75 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
+  //use extend function  
+  
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  /*
+  look through tree to check for value
+  check top node for target value
+    if node.value does not equal target
+      iterate over children
+        check target value of current node
+          if current node value === target
+            return true
+          check if current node has children
+            if so, use recursion
+
+      return false
+  */
+  // var recursion = function(node) {
+  //   console.log(node, this, 'what is this???');
+  //   if (node.value === target) {
+  //     return true;
+  //   }
+  //   if (node.children.length > 0) {
+  //     for (var i = 0; node.children.length; i++) {
+  //       if (node.children[i].value === target) {
+  //         return true;
+  //       }
+  //       if (node.children[i].children.length > 0) {
+  //         recursion(node.children[i])
+  //       }
+  //     }
+  //   }
+  //   return false;
+  // }
+  // recursion(this);
+
+
+  var node = this
+  console.log(node, 'what is this?????')
+  console.log(this.children[0].children);
+  if (node.value === target) {
+    return true;
+  } 
+  else if (node.children.length > 0) {
+  
+    for (var i = 0; node.children.length; i++) {
+      var currentNode = node.children[i];
+      if (currentNode.value === target) {
+        return true;
+      }
+      if (currentNode.children.length > 0) {
+        console.log(this.children[i])
+        this.children[i].contains(target);
+      }
+    }
+   
+  }
+  return false;
 };
 
 
