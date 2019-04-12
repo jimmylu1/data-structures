f12be7699c62d9920aae51694136a3c8d68afcff
jimmylu1 <jimmy88lu@gmail.com>
2019-04-12 15:15:12 -0700

    Implement solution for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  /*
+  make item as key in set._storage and value as item
+  */
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  //if item is in set, return true else return false
+
+  return this._storage[item] === item;
 };
 
 setPrototype.remove = function(item) {
+  // delete item
+  delete this._storage[item];
 };
 
 /*
