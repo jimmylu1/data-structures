c517b7c361cc634f8043cce9c34680e1c2e905aa
jimmylu1 <jimmy88lu@gmail.com>
2019-04-12 21:48:39 -0700

    Implement solution for hashTable
    Clean up code for tree.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,49 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+    this._storage.each(function(element, i) {
+      if (index === i) {
+        element.push([k, v]);
+      }
+    });
+  } else {
+    this._storage.each(function(element, i) {
+      if (index === i) {
+        for (var j = 0; j < element.length; j++) {
+          if (element[j][0] === k) {
+            element[j][1] = v;
+          } else {
+            element.push([k, v]);
+          }
+        }
+      }
+    });
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(element, i) {
+    if (i === index) {
+      for (var j = 0; element.length; j++) {
+        if (k === element[j][0]) {
+          element.splice(j,1);
+        }
+      }
+    }
+  });
 };
 
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,13 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = [];  // fix me
+  newTree.children = [];
   _.extend(newTree, treeMethods);
-  return newTree;
-  //use extend function  
-  
+  return newTree; 
 };
 
 var treeMethods = {};
@@ -18,57 +14,20 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  /*
-  look through tree to check for value
-  check top node for target value
-    if node.value does not equal target
-      iterate over children
-        check target value of current node
-          if current node value === target
-            return true
-          check if current node has children
-            if so, use recursion
-
-      return false
-  */
-  // var recursion = function(node) {
-  //   console.log(node, this, 'what is this???');
-  //   if (node.value === target) {
-  //     return true;
-  //   }
-  //   if (node.children.length > 0) {
-  //     for (var i = 0; node.children.length; i++) {
-  //       if (node.children[i].value === target) {
-  //         return true;
-  //       }
-  //       if (node.children[i].children.length > 0) {
-  //         recursion(node.children[i])
-  //       }
-  //     }
-  //   }
-  //   return false;
-  // }
-  // recursion(this);
-
-
-  var node = this
-  console.log(node, 'what is this?????')
-  console.log(this.children[0].children);
-  if (node.value === target) {
+  if (this.value === target) {
     return true;
   } 
-    for (var i = 0; i < node.children.length; i++) {
-      var currentNode = node.children[i];
-      if (currentNode.value === target) {
-        return true;
-      }
-      if (currentNode.children.length > 0) {
-        console.log(this.children[i])
-        if(this.children[i].contains(target)){
-          return true;
-        };
-      }
+  for (var i = 0; i < this.children.length; i++) {
+    var currentNode = this.children[i];
+    if (currentNode.value === target) {
+      return true;
     }
+    if (currentNode.children.length > 0) {
+      if(this.children[i].contains(target)){
+        return true;
+      };
+    }
+  }
   return false;
 };
 
